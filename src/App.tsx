658d706b007706e52a57b18d03be72be0cdc9735
Felipe <felipecsilva.dev@gmail.com>
2026-06-28 11:16:36 -0300

    Implementando o CSS module para que haja independencia das classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1>ola mundo</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam minus
         non obcaecati laborum. Voluptate aliquam accusamus eum assumenda sequi
