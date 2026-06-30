a9d241ef2b4ea5980cf79d9fa059bcff2446712e
Felipe <felipecsilva.dev@gmail.com>
2026-06-29 22:10:13 -0300

    Last commit day

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
@@ -5,13 +6,9 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam minus
-        non obcaecati laborum. Voluptate aliquam accusamus eum assumenda sequi
-        corrupti atque! Rerum dolor iure adipisci velit exercitationem
-        accusantium molestias autem.
-      </p>
+      <Container>
+        <Heading>Ola</Heading>
+      </Container>
     </>
   );
 }
