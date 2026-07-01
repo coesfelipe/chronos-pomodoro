f9840def2c567bf9c862a169be45463aeb53ce51
Felipe <felipecsilva.dev@gmail.com>
2026-07-01 10:21:29 -0300

    Implementaçao da logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Ola</Heading>
+        <Logo />
       </Container>
     </>
   );
